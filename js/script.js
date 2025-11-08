b8218c49a2b1c714b604ab4d254de5767115cdfe
beshoywadid <beshoywadidfayez@gmail.com>
2025-11-08 22:35:18 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ let secondCard = 1;
 let sum = firstCard + secondCard;
 
 console.log(sum);
+
+
+let age = 22;
+if (age < 22){
+    console.log("You can not enter the club!");
+}
+else {
+    console.log("Welcome !")
+}
