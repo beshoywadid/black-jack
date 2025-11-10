bb110948298b7b5e54f51de27f476f85a7c49211
beshoywadid <beshoywadidfayez@gmail.com>
2025-11-10 23:13:46 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,27 @@ let firstCard = 10;
 let secondCard = 7;
 let sum = firstCard + secondCard;
 let isAlive = true;
+let message = "";
 
 if (sum <= 20){
-    console.log("Do you want to draw a new card? 🙂");
+    message = "Do you want to draw a new card? 🙂";
 }
 else if(sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳");
+    message = "Wohoo! You've got Blackjack! 🥳";
 }
 else {
-    console.log("You're out of the game! 😭");
+    message = "You're out of the game! 😭";
     isAlive = false;
 }
 
-console.log(isAlive);
+console.log(message);
+
+/* 
+    false
+    true
+    false
+    false
+    true
+    true
+    false
+*/
